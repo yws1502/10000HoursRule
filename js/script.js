312a7cbe9d3bcd39547dcd433f019b35ddec57f9
Woosang <yws1502@naver.com>
2021-12-30 11:17:37 +0900

    ✨ Create show modal section

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 const inputCont = document.querySelector(".input-container");
 const btnCheck = inputCont.querySelector(".btn-check");
 
-
-btnCheck.addEventListener("click", () => {
+function ruleCalc () {
   const skillInput = inputCont.querySelector("#Skill");
   const timeInput = inputCont.querySelector("#Time");
   const errorMsg = inputCont.querySelector(".input-error");
   const resultCont = document.querySelector(".result-container");
 
+  // 유효성 검사
   if (skillInput.value && timeInput.value) {
     const skillText = resultCont.querySelector(".skill");
     const timeText = resultCont.querySelector(".time");
@@ -23,4 +23,12 @@ btnCheck.addEventListener("click", () => {
     // 입력값을 없는 경우 처리
     errorMsg.style.display = "block";
   }
+}
+
+const modal = document.querySelector(".modal-background");
+
+document.querySelector(".btn-go").addEventListener("click", () => {
+  modal.style.display = "block";
 })
+
+btnCheck.addEventListener("click", ruleCalc);
